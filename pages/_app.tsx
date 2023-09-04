86509023925bf2a0e0a76ad49029fcd68855e40d
hoangh3 <hoangtnth@gmail.com>
2023-09-04 13:25:07 +0700

    edge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import { AppProps } from 'next/app'
 import '../styles/index.css'
 
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
