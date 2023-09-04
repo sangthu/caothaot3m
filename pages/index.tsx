ddcccc5c848410ff452b355accce379aa9a38d9b
hoangh3 <hoangtnth@gmail.com>
2023-09-04 13:30:18 +0700

    1

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 
+export const runtime = 'experimental-edge'
+
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(1)
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -14,6 +14,8 @@ import Tags from '../../components/tags'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import { CMS_NAME } from '../../lib/constants'
 
+export const runtime = 'experimental-edge'
+
 export default function Post({ post, posts, preview }) {
   const router = useRouter()
   const morePosts = posts?.edges
